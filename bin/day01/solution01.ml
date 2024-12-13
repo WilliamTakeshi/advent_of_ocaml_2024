8107fd941a690c8a84f97e2c95f1de6e70c9385d
William Takeshi <williamtpereira@gmail.com>
2024-12-13 08:32:03 -0300

    day01 part1

--- bin/day01/solution01.ml
+++ bin/day01/solution01.ml
@@ -10,8 +10,23 @@ let read_file filename =
   in
   read_lines []
 
+let parse_line line =
+  match String.split_on_char ' ' line |> List.filter ((<>) "") |> List.map int_of_string with
+  | [x; y] -> (x, y)
+  | _ -> failwith "Invalid input format"
+
+let split_pairs pairs =
+  List.fold_right (fun (x, y) (left, right) -> (x :: left, y :: right)) pairs ([], [])
+
+let total_distance left right =
+  let sorted_left = List.sort compare left in
+  let sorted_right = List.sort compare right in
+  List.fold_left2 (fun acc x y -> acc + abs (x - y)) 0 sorted_left sorted_right
+
 let () =
   let filename = "./bin/day01/test.txt" in
-  match read_file filename with
-  | [] -> Printf.printf "The file is empty.\n"
-  | lines -> List.iter print_endline lines
+  let lines = read_file filename in
+  let pairs = List.map parse_line lines in
+  let (left, right) = split_pairs pairs in
+  let distance = total_distance left right in
+  Printf.printf "Total distance: %d\n" distance
